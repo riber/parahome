bc7e7043def57b6f018b8bc7128e432233ee7858
Nanyan <nly85@qq.com>
2012-04-21 22:04:54 +0800

    页面的一些基本布局，未完成。测试Git相关操作

---
+++ Code/ParadiseHome/ParadiseHome.Common/Utils/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParadiseHome.Common.Utils
+{
+    public class Constants
+    {
+        /// <summary>
+        /// 常量：主程序名。主界面标题按如下格式显示：模块名 - 主程序名
+        /// </summary>
+        public static readonly string MainWinTitle = "**福位管理系统";
+    }
+}
